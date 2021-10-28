704b673badf19b2a2c2ade6dd870a91ad3eb280c
Daniel Martinez-Gamboa <dmartinezgamboa@gmail.com>
2021-10-28 05:12:19 -0700

    First commit

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,83 +1,58 @@
 export const getOne = model => async (req, res) => {
   try {
-    const doc = await model
-      .findOne({ createdBy: req.user._id, _id: req.params.id })
-      .lean()
-      .exec()
-
+    const id = req.params.id
+    const userId = req.user._id
+    const doc = await model.findOne({ _id: id, createdBy: userId }).exec()
     if (!doc) {
       return res.status(400).end()
     }
-
-    res.status(200).json({ data: doc })
+    return res.status(200).json({ data: doc })
   } catch (e) {
     console.error(e)
-    res.status(400).end()
   }
 }
 
 export const getMany = model => async (req, res) => {
-  try {
-    const docs = await model
-      .find({ createdBy: req.user._id })
-      .lean()
-      .exec()
-
-    res.status(200).json({ data: docs })
-  } catch (e) {
-    console.error(e)
-    res.status(400).end()
-  }
+  const userId = req.user._id
+  const doc = await model.find({ createdBy: userId }).exec()
+  return res.status(200).json({ data: doc })
 }
 
 export const createOne = model => async (req, res) => {
-  const createdBy = req.user._id
   try {
-    const doc = await model.create({ ...req.body, createdBy })
-    res.status(201).json({ data: doc })
+    const userId = req.user._id
+    const doc = await model.create({ ...req.body, createdBy: userId })
+    return res.status(201).json({ data: doc })
   } catch (e) {
     console.error(e)
-    res.status(400).end()
   }
 }
 
 export const updateOne = model => async (req, res) => {
   try {
-    const updatedDoc = await model
-      .findOneAndUpdate(
-        {
-          createdBy: req.user._id,
-          _id: req.params.id
-        },
-        req.body,
-        { new: true }
-      )
-      .lean()
+    const id = req.params.id
+    const userId = req.user._id
+    const doc = await model
+      .findOneAndUpdate({ _id: id, createdBy: userId }, req.body, { new: true })
       .exec()
-
-    if (!updatedDoc) {
+    if (!doc) {
       return res.status(400).end()
     }
-
-    res.status(200).json({ data: updatedDoc })
+    return res.status(200).json({ data: doc })
   } catch (e) {
     console.error(e)
-    res.status(400).end()
   }
 }
 
 export const removeOne = model => async (req, res) => {
   try {
-    const removed = await model.findOneAndRemove({
-      createdBy: req.user._id,
-      _id: req.params.id
-    })
-
-    if (!removed) {
+    const id = req.params.id
+    const userId = req.user._id
+    const doc = await model.findOneAndRemove({ _id: id, createdBy: userId })
+    if (!doc) {
       return res.status(400).end()
     }
-
-    return res.status(200).json({ data: removed })
+    return res.status(200).json({ data: doc })
   } catch (e) {
     console.error(e)
     res.status(400).end()
